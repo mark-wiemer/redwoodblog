de1f2a9d85f326d0aeea16a61ce5a2d072beef18
Mark Wiemer <markwiemer@outlook.com>
2022-07-04 19:32:58 -0700

    Our First Cell
    https://redwoodjs.com/docs/tutorial/chapter2/cells

---
+++ web/src/components/ArticlesCell/ArticlesCell.tsx
@@ -0,0 +1,29 @@
+import type { ArticlesQuery } from 'types/graphql'
+
+import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
+
+export const QUERY = gql`
+  query ArticlesQuery {
+    posts {
+      id
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }: CellFailureProps) => (
+  <div style={{ color: 'red' }}>Error: {error.message}</div>
+)
+
+export const Success = ({ posts }: CellSuccessProps<ArticlesQuery>) => {
+  return (
+    <ul>
+      {posts.map((item) => {
+        return <li key={item.id}>{JSON.stringify(item)}</li>
+      })}
+    </ul>
+  )
+}
